a43325577d88888e99b48bc6adb66852610992b6
alexvorn <voroncov.sasha93@gmail.com>
2023-02-01 18:34:02 +0200

    delete push user in saveFile, add addUser

--- server/src/services/db/db.service.ts
+++ server/src/services/db/db.service.ts
@@ -1,21 +1,37 @@
 import { response } from 'express';
 import { json } from 'node:stream/consumers';
 
-
+const urlToSave = './src/services/db/users.json';
 const fs = require('file-system');
 
 
-function getFile() {
-    return JSON.parse(fs.readFileSync('./src/services/db/users.json'));  
+function getFile(file) {
+    return JSON.parse(fs.readFileSync(file));  
 }
 
-function saveFile(user) {
-    const Users = getFile();
-    Users.push(user)
-    fs.writeFile('./src/services/db/users.json', JSON.stringify(Users, null, 2))
+function saveFile(urlToSave, file) {
+    if (!file || !urlToSave) {
+        throw('Cant save file');
+    } 
+    fs.writeFile(urlToSave, JSON.stringify(file, null, 2))
 }
-function creatUser(user) {
-
+function returnLastItem(arr) {
+    return arr[arr.length - 1];
 }
 
-saveFile({"name" : "san9"})
+function addUser(user) {
+    const users = getFile(urlToSave);
+    const lastItem = returnLastItem(users);
+    user.id = lastItem ? user.id = lastItem.id + 1 || user.id : 1;
+    
+    users.forEach(e => {
+        if(e.name === user.name) {
+           throw('пользователь с такими данными уже зарегестрирован');
+        }
+    });
+    users.push(user);
+    saveFile(urlToSave, users)
+}
+
+
+addUser({"name" : "radik"})
