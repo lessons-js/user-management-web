b6bccf256fccfb4d8c21a7bb181428f2ae719156
kali <kali@localhost.localdomain>
2023-02-07 16:40:57 -0600

    make redirekt  (/ => /users)

--- client/src/routing/Router.jsx
+++ client/src/routing/Router.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "../components/header";
-import Home from "../pages/home";
+//import Home from "../pages/home";
+import Users from "../pages/home/Users";
+
 
 const Router = () => (
   <BrowserRouter>
     <Header />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Users />} />
+      <Route path="/users" element={<Users />} />
     </Routes>
   </BrowserRouter>
 );
 
 export default Router;
+
