ee8b32445fb9ca1e87f2fb16c0c4ce5d549b32f6
Radion Volovoi <Radikvolovoy@gmail.com>
2023-02-14 15:34:47 +0200

    added endpoints and validation

--- server/src/routes/hello.ts
+++
@@ -1,3 +0,0 @@
-export const hello = (req, res) => {
-  res.send('hello world')
-}
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
-import { hello } from './hello';
+import express from "express";
+import { users, create, update, deleteUserById } from "./UserController";
+import "../services/db/db.service";
 
 const routes = express.Router();
 
-routes.route('/hello').get(hello); 
+routes.route("/users").get(users);
+routes.route("/users").post(create);
+routes.route("users/:userId").delete(deleteUserById);
+routes.route("users/:userId").put(update);
 
 export default routes;
