f6b1e4ad2444459ff4b2e3eef5df25204821a951
Andrii Starusiev <andrejstarusev@gmail.com>
2023-03-01 16:24:54 +0200

    Added fake endpoints

---
+++ client/src/mocks/fakeEndpoints.js
@@ -0,0 +1,28 @@
+const mockUsers = [{ "id": 1, "userName": "Oleg", "email": "oleg@gmail.com", "phoneNumber": 360507076591, "name": "Oleg", "phone": 360507076591 }, { "id": 2, "userName": "Max", "email": "Max@gmail.com", "phoneNumber": 3606678909 }, { "id": 3, "userName": "Viktor", "email": "Viktor@gmail.com", "phoneNumber": 3806622261986 }];
+const fakeErrorResponse = {
+  error: 'Oops, some error happend',
+}
+
+export function getUsersWithDelay(delay = 3000, returnSuccess = true) {
+  return new Promise((res, rej) => {
+    setTimeout(() => returnSuccess ? res({ data: mockUsers }) : rej(fakeErrorResponse), delay);
+  });
+}
+
+export function updateUsersWithDelay(userToUpdate, delay = 3000, returnSuccess = true) {
+  return new Promise((res, rej) => {
+    setTimeout(() => returnSuccess ? res({ data: userToUpdate }) : rej(fakeErrorResponse), delay);
+  });
+}
+
+export function deleteUserWithDelay(userId, delay = 3000, returnSuccess = true) {
+  return new Promise((res, rej) => {
+    setTimeout(() => returnSuccess ? res({ data: { ok: true } }) : rej(fakeErrorResponse), delay);
+  });
+}
+
+export function addUserWithDelay(user, delay = 3000, returnSuccess = true) {
+  return new Promise((res, rej) => {
+    setTimeout(() => returnSuccess ? res({ data: user }) : rej(fakeErrorResponse), delay);
+  });
+}
