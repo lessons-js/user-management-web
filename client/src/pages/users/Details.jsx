52c06894f1da667aa6fc69c713884ab95012aede
Makar <makarlrom1@gmail.com>
2023-04-16 10:04:02 +0300

    fix

--- client/src/pages/users/Details.jsx
+++ client/src/pages/users/Details.jsx
@@ -3,9 +3,9 @@ import { useParams } from "react-router-dom";
 function Details() {
     const { id } = useParams()
     const selectedUser = "";
-    fetch(`localhost:3001/users/${id}`),{ method: 'POST', headers: { 'content-type': 'application/json' }}.then(res => res.json()).then(res => {
+    fetch(`localhost:3001/users/${id}`,{ method: 'POST', headers: { 'content-type': 'application/json' }}.then(res => res.json()).then(res => {
         selectedUser(res.currentUser)
-      })
+      }))
 }
 
 export default Details;
