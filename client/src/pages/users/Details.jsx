633e1f66c5d40a5ccb943457ceb02fdb64c5e493
Makar <makarlrom1@gmail.com>
2023-04-16 09:43:55 +0300

    fetch add

--- client/src/pages/users/Details.jsx
+++ client/src/pages/users/Details.jsx
@@ -2,6 +2,10 @@ import { useParams } from "react-router-dom";
 
 function Details() {
     const { id } = useParams()
+    const selectedUser = "";
+    fetch(`localhost:3001/users/${id}`),{ method: 'POST', headers: { 'content-type': 'application/json' }}.then(res => res.json()).then(res => {
+        selectedUser(res.currentUser)
+      })
 }
 
 export default Details;
